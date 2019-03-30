de296508a091a4e105e3fabb75c8a93b2ea9dfc1
Beau <cummingsbeau@gmail.com>
2019-03-30 10:46:43 -0700

    added all files

---
+++ client/build/precache-manifest.440ca7fb3d16ce83bfc94a74b758e7cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "738fb0650434015f4aa2",
+    "url": "/static/js/main.20e838fc.chunk.js"
+  },
+  {
+    "revision": "19f0dc92cc37bea57c9d",
+    "url": "/static/js/2.4d4d1d51.chunk.js"
+  },
+  {
+    "revision": "738fb0650434015f4aa2",
+    "url": "/static/css/main.c31b6042.chunk.css"
+  },
+  {
+    "revision": "ef066054d666fa71495320e743c9f8a3",
+    "url": "/index.html"
+  }
+];
